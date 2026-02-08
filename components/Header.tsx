b87caf623bbbf326c2c883bb90ab1c474ecbe06e
SAN.D <das95sankaram@gmail.com>
2026-02-08 23:35:26 +0530

    header complete

---
+++ components/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "@/components/NavItems";
+import UserDropdown from "@/components/UserDropdown";
+
+const Header = () => {
+    return (
+        <header className='sticky top-0 header'>
+            <div className='container header-wrapper'>
+                <Link href='/'>
+                    <Image src='/assets/icons/logo.svg' alt='Stock-Stack logo' width={120} height={32} className='h-8 w-auto cursor-pointer'/>
+                </Link>
+                <nav className='hidden sm:block'>
+                    {/*NavItems*/}
+                    <NavItems />
+                </nav>
+                {/*UserDropdown*/}
+                <UserDropdown user={undefined} initialStocks={[]} />
+            </div>
+        </header>
+    )
+}
+export default Header
+
+// 38:22
